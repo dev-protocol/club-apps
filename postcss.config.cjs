f388e36942b9c733c634b170507f0a35bce7d159
Matteu <matteugt@gmail.com>
2022-07-30 02:33:34 +0800

    UPDATE: Temporarily disable purgecss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,9 @@
-const hashi = require('@devprotocol/hashi/config/purgecss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+// const hashi = require('@devprotocol/hashi/config/purgecss')
+// const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    purgecss(
-      hashi.purgeConfig({
-        content: ['**/*.astro', '**/*.vue'],
-      })
-    ),
   ],
 }
