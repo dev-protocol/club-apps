0f0b58f959041741bd508fecac94c0555155d6c0
Matteu <matteugt@gmail.com>
2022-07-30 02:22:23 +0800

    FEAT: Add purgecss to project

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,14 @@
+const hashi = require('@devprotocol/hashi/config/purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss(
+      hashi.purgeConfig({
+        content: ['**/*.astro', '**/*.vue'],
+      })
+    ),
+  ],
 }
